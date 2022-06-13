8faac5e9ae1b242e2d9462c3cf014395fc769bd6
Thibe Provost <thibe.provost@viso-cor-mariae.be>
2022-06-13 11:35:35 +0200

    Add project files.

---
+++ Oef3-Thibe/Dier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oef3_Thibe
+{
+    abstract class Dier
+    {
+        public double dblGewicht;
+        public int intAatalPoten;
+        public Dier()
+        {
+            dblGewicht = 0;
+            intAatalPoten = 0; 
+        }
+        public abstract void Geluid();
+
+    }
+}
---
+++ Oef3-Thibe/Koe.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oef3_Thibe
+{
+    class Koe : Dier
+    {
+        private string strRas;
+        private string strNaam;
+        private string strAantalLiterMelk;
+
+        public Koe()
+        {
+            strRas = "Onbekent";
+            strNaam = "Onbekent";
+            strAantalLiterMelk = "Hoeveeleid melk onbekend";
+        }
+        public string strras  
+        {
+            get { return strRas; }   
+            set { strRas = value; }  
+        } 
+        public string strnaam  
+        {
+            get { return strNaam; }   
+            set { strNaam = value; }  
+        } 
+        public string straantalLiterMelk  
+        {
+            get { return strAantalLiterMelk; }   
+            set { strAantalLiterMelk = value; }  
+        }
+        public override void Geluid()
+        {
+            Console.WriteLine("De koe loeit");
+        }
+    }
+}
